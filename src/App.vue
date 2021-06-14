40ca31a7779e371695ddda7c16885e0bfe6d8f94
VigilBushido <vigilbushido@gmail.com>
2021-06-13 20:53:42 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Exercise />
+</template>
+
+<script>
+import Exercise from './components/Exercise1-01'
+
+export default {
+  components: {
+    Exercise,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
