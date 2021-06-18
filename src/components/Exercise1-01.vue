d615690664c52b7f3a9b952728bcfab7dc691cf2
vigilbushido <vigilbushido@gmail.com>
2021-06-18 14:30:33 -0400

    jsconifg added

--- src/components/Exercise1-01.vue
+++ src/components/Exercise1-01.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
+  <section>
     <h1 v-show="true" v-once v-text="text">Loading...</h1>
     <h2 v-show="false" v-html="html" />
-
+    <h3>Looping through arrays</h3>
+    <ul>
+      <li v-for="n in 5" :key="n">
+        {{ n }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(item, n) in interest" :key="n">
+        {{ item }}
+      </li>
+    </ul>
     <a
       :href="link.url"
       :target="link.target"
       :tabindex="link.tabindex"
       v-text="link.title"
     />
-  </div>
+    <div class="form">
+      <label>
+        <input type="text" v-model="name" />
+      </label>
+      <label>
+        Preferred javascript style
+        <select name="language" v-model="language">
+          <option value="Javascript">JavaScript</option>
+          <option value="TypeScript">TypeScript</option>
+          <option value="CoffeeScript">CoffeeScript</option>
+          <option value="Dart">Dart</option>
+        </select>
+      </label>
+    </div>
+    <ul class="overview">
+      <li><strong>Overview</strong></li>
+      <li>Name: {{ name }}</li>
+      <li>Preference: {{ language }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -27,12 +56,46 @@ export default {
         tabindex: '0',
         title: 'Go to Google',
       },
+      name: '',
+      language: '',
+      interest: [
+        'Giannis Antekounpo',
+        'Path of Exile',
+        'Full Stack Java Programming',
+      ],
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.form {
+  display: flex;
+  justify-content: space-evenly;
+  max-width: 800px;
+  padding: 40px 20px;
+  border-radius: 10px;
+  margin: 0 auto;
+  background: #ececec;
+}
+
+.overview {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  max-width: 300px;
+  margin: 40px auto;
+  padding: 40px 20px;
+  border-radius: 10px;
+  border: 1px solid #ececec;
+
+  > li {
+    list-style: none;
+    + li {
+      margin-top: 20px;
+    }
+  }
+}
 h2 {
   margin: 40px 0 0;
   font-weight: normal;
