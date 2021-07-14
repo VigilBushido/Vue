517dd5ea8eca092c7bec4aa217d4ff8358b9500e
vigilbushido <vigilbushido@gmail.com>
2021-07-13 23:40:47 -0400

    delete first fixed

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="form">
+  <div class="container">
+    <h1>Shopping List</h1>
     <label>
-      <input type="text" v-model="itemToShop" />
+      <input
+        placeholder="Press enter to add new item"
+        v-model="input"
+        @keyup.enter="addItem"
+        ref="input"
+      /><button @click="addItem">Add item</button>
     </label>
+
     <label>
       Level of Priority:
       <select name="priority" v-model="priority">
@@ -12,63 +19,146 @@
         <option value="Can Wait">Can Wait</option>
       </select>
     </label>
+
     <ul class="overview">
       <li><strong>Shopping List</strong></li>
-      <li>Item To Buy: {{ itemToShop }}</li>
+      <li>Item To Buy: {{ input }}</li>
       <li>Priority Level: {{ priority }}</li>
     </ul>
+    <!--     <ul class="left">
+      <strong>Official List</strong>
+      <li v-for="(item, n) in shoppingList" :key="n">
+        {{ item }} <b @click="deleteItem(item)">Delete</b>
+      </li>
+    </ul> -->
+    <ul v-if="shoppingList">
+      <li v-for="(item, i) in shoppingList" :key="i" class="item"
+        ><span> {{ item }}</span>
+        <button class="button--remove" @click="deleteItem(i)">Remove</button>
+      </li>
+    </ul>
+
+    <br />
+    <button class="button--delete" @click="deleteItem()">Delete ALL</button>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    addItem(value) {
-      this.shoppinglist = this.shoppinglist.add(value)
-    },
-    deleteItem(value) {
-      this.shoppinglist = this.shoppinglist.filter(
-        itemToShop => itemToShop !== value
-      )
-    },
-  },
   data() {
     return {
-      shoppinglist: [],
-      itemToShop: '',
+      input: '', //itemToShop: '',
+      shoppingList: [],
       priority: '',
     }
   },
+  methods: {
+    addItem() {
+      //this.shoppingList = this.shoppingList.push(value)
+      //no adding to the list if empty input
+      if (!this.input) return
+      this.shoppingList.push(this.input)
+      //clear input afterwards
+      this.input = ''
+      //focus the input element for quick typing!
+      this.$refs.input.focus()
+    },
+    deleteItem(i) {
+      if (i == 0) {
+        this.shoppingList = this.shoppingList.filter((item, x) => x !== i)
+        console.log(this.shoppingList)
+        console.log(i)
+      }
+      //this.shoppingList = this.shoppingList.filter(item => item !== value)
+
+      //filter out that array item and update the shoppingList prop;
+      //otherwise replace the data prop with an empty array
+      else if (i != 0)
+        this.shoppingList = i
+          ? this.shoppingList.filter((item, x) => x !== i)
+          : []
+    },
+  },
 }
 </script>
-<style lang="scss" scoped>
-.form {
-  display: flex;
-  justify-content: space-evenly;
-  max-width: 800px;
-  padding: 40px 20px;
-  border-radius: 10px;
-  margin: 0 auto;
-  background: #ececec;
+
+<style lang="scss">
+@import 'src/styles/global';
+
+$color-green: #4fc08d;
+$color-grey: #2c3e50;
+
+.container {
+  max-width: 600px;
+  margin: 80px auto;
 }
 
-.overview {
-  position: absolute;
-  right: 0px;
+.item {
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  max-width: 300px;
-  margin: 40px auto;
-  padding: 40px 20px;
+  justify-content: space-between;
+}
+
+// Type
+.h2 {
+  font-size: 21px;
+}
+
+.user-input {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  input {
+    width: 100%;
+    padding: 10px 6px;
+    margin-right: 10px;
+  }
+}
+
+.item {
+  display: flex;
+  align-items: center;
+}
+
+// Buttons
+button {
+  appearance: none;
+  padding: 10px;
+
+  font-weight: bold;
   border-radius: 10px;
-  border: 1px solid #ececec;
+  border: none;
+  background: $color-grey;
+  color: white;
+  white-space: nowrap;
+
+  + button {
+    margin-left: 10px;
+  }
+}
+
+.button--delete {
+  display: block;
+  margin: 0 auto;
+  background: red;
+}
+
+.button--remove {
+  background: none;
+  color: red;
+  text-transform: uppercase;
+  font-size: 11px;
+  align-self: flex-end;
+}
+
+ul {
+  display: block;
+  margin: 0 auto;
+  padding: 30px;
+  border: 1px solid rgba(0, 0, 0, 0.25);
 
   > li {
-    list-style: none;
-    + li {
-      margin-top: 20px;
-    }
+    color: $color-grey;
+    margin-bottom: 4px;
   }
 }
 </style>
