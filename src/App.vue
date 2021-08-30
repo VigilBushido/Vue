7b8a1b319dd514e24005b79957fb9339a2810b66
vigilbushido <vigilbushido@gmail.com>
2021-08-30 16:34:16 -0400

    ApiBlog final commit

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <!-- <Exercise /> -->
     <!-- <ShoppingCart /> -->
-    <Exercise2 />
+    <!-- <Exercise2 /> -->
+    <ApiBlog />
   </div>
 </template>
 
 <script>
 //import Exercise from './components/Exercise1-01'
-import Exercise2 from './components/Exercise2-01'
+//import Exercise2 from './components/Exercise2-01'
+import ApiBlog from './components/ApiBlog.vue'
 //import ShoppingCart from './components/ShoppingCart'
 
 export default {
   name: 'app',
   components: {
-    Exercise2, //ShoppingCart, //Exercise,
+    //Exercise2,
+    ApiBlog, //ShoppingCart, //Exercise,
   },
 }
 </script>
